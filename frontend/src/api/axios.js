05c1b6e41684701c13028b2763c1c72c1fa907a6
asmit-kushwaha <as.ashmitkushwaha2000@gmail.com>
2026-07-25 12:31:25 +0530

    Update axios.js
    Updated the base url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://asmit-byt5.onrender.com/api',
   withCredentials: true, // needed later for JWT cookies
 });
 
