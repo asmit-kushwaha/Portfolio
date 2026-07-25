7687737fc0b4500dd2685e6a569926a18896eb18
asmit-kushwaha <as.ashmitkushwaha2000@gmail.com>
2026-07-25 13:00:15 +0530

    Update authController.js
    changes the re.cookie

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,11 +8,11 @@ const generateTokenAndSetCookie = (res, userId) => {
   });
 
   res.cookie('token', token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-  });
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production',
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
 };
 
 // @desc   Register admin (only run this ONCE to create your account)
