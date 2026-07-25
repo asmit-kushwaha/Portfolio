52fd3b8a3d29262db5f9b14bb44272697dd73e30
asmit-kushwaha <as.ashmitkushwaha2000@gmail.com>
2026-07-25 20:10:11 +0530

    Update server.js
    Render deploy Error resolved

--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,3 @@ app.use('/api/settings', settingsRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-app.use('/api/settings', settingsRoutes);
- 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
